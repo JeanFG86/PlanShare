b1def04720321d18e898973d3d88a70b216f8a77
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2025-10-26 16:43:22 -0300

    use fluent migration

--- src/Backend/PlanShare.Api/Program.cs
+++ src/Backend/PlanShare.Api/Program.cs
@@ -5,6 +5,8 @@ using PlanShare.Api.Token;
 using PlanShare.Application;
 using PlanShare.Domain.Security.Tokens;
 using PlanShare.Infrastructure;
+using PlanShare.Infrastructure.Extensions;
+using PlanShare.Infrastructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -75,4 +77,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+if(builder.Configuration.IsUnitTestEnviroment() is false)
+{
+    await MigrationDatabase();
+}
+
 app.Run();
+
+async Task MigrationDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+    DatabaseMigration.MigrateDatabase(scope.ServiceProvider);
+}
---
+++ src/Backend/PlanShare.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PlanShare.Infrastructure.Migrations;
+
+public static class DatabaseMigration
+{
+    public static void MigrateDatabase(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+        runner.ListMigrations();
+        runner.MigrateUp();
+    }
+}
